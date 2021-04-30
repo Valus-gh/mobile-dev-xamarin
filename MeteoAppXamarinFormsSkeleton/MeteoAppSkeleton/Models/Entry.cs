0ee185225e0a78c5f301c00340df21b4f7469295
Simone Belingheri <simone.belingheri@student.supsi.ch>
2021-04-30 12:57:37 +0200

    initial commit

---
+++ MeteoAppXamarinFormsSkeleton/MeteoAppSkeleton/Models/Entry.cs
@@ -0,0 +1,12 @@
+﻿namespace MeteoAppSkeleton.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int AvgTemp { get; set; }
+        public int LowestTemp { get; set; }
+        public int HighestTemp { get; set; }
+
+    }
+}
